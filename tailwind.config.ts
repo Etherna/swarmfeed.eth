3dbe32302401956a1c23a2e93eb9d62acbf0e6ba
Mattia Dalzocchio <mattiadalzocchio@me.com>
2023-07-31 10:34:01 +0200

    add missing animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,10 +61,20 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "tick-fade": {
+          from: { opacity: "1" },
+          to: { opacity: "0.1" },
+        },
+        "tick-bounce": {
+          from: { transform: "translateX(-180%)" },
+          to: { transform: "translateX(180%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "tick-fade": "tick-fade 1.2s infinite",
+        "tick-bounce": "tick-bounce 1.25s infinite alternate linear",
       },
     },
   },
